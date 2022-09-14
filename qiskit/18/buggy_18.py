f15c5b94611c0cbd20e0588cf20a0aa4ee889d42
Z-928 <56246485+Z-928@users.noreply.github.com>
2022-09-15 00:48:38 +0900

    Update buggy_18.py

--- qiskit/18/buggy_18.py
+++ qiskit/18/buggy_18.py
@@ -1,25 +1,11 @@
-import unittest
 from qiskit import *
 
-class Test(unittest.TestCase):
-
-    
-    def test_b18(self):
-        try:
-            if __name__ == "__main__":
-                backend = Aer.get_backend('statevector_simulator')
-                qc = QuantumCircuit(2, 2)
-                qc = QuantumCircuit(2, 2)
-                qc.h(0)
-                qc.cx(0, 1)
-                qc.measure([0,1], [0,1])
-                result = execute(qc, backend, 100).result()
-                print(result.get_counts(qc))
-        except TypeError as e:
-            self.assertEqual(type(e), TypeError)
-           
-        else:
-            self.fail('TypeError not raised')
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    backend = Aer.get_backend('statevector_simulator')
+    qc = QuantumCircuit(2, 2)
+    qc = QuantumCircuit(2, 2)
+    qc.h(0)
+    qc.cx(0, 1)
+    qc.measure([0,1], [0,1])
+    result = execute(qc, backend, 100).result()
+    print(result.get_counts(qc))
